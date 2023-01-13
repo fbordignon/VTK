0227875f009ce4d7632b43ca84e5552ed6d3c753
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-01-13 06:56:24 -0500

    [SlicerVirtualReality] BUG: Ensure a valid OpenVR pose is returned
    This commit fixes a regression introduced in previous commit.
    
    Co-authored-by: Lucas Gandel <lucas.gandel@kitware.com>

--- Rendering/OpenVR/vtkOpenVRRenderWindow.cxx
+++ Rendering/OpenVR/vtkOpenVRRenderWindow.cxx
@@ -552,7 +552,10 @@ void  vtkOpenVRRenderWindow::GetOpenVRPose(
   {
     *pose = &(this->OpenVRTrackedDevicePoses[handle]);
   }
-  *pose = nullptr;
+  else
+  {
+    *pose = nullptr;
+  }
 }
 
 VTK_ABI_NAMESPACE_END
